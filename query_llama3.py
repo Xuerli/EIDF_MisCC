ec8ca86f1458da73083219f4b8ed5939cb356e35
Xue Li <xli3310@ed.ac.uk>
2024-09-05 16:43:57 +0100

    Create query_llama3.py

---
+++ query_llama3.py
@@ -0,0 +1,77 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+import os
+import json
+
+
+if __name__ == "__main__":
+    # Retrieve arguments
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("hf_token", "--string", type=str, required=True)
+
+    args = argument_parser.parse_args()
+
+    cache_dir = "/mnt/query-llama3/hf_cache"
+    token = args.hf_token
+    dataset_file = "/mnt/query-llama3/toy_trees.json"
+    model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+    output_file = "/mnt/query-llama3/predictions.jsonl"
+
+
+    with open(dataset_file, "r") as f:
+        instances = json.load(f)
+
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id, token=token, cache_dir=cache_dir)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+        token=token,
+        cache_dir=cache_dir
+    )
+
+    messages = [{"role": "system", "content": "You are a system for content moderation. You analyse hateful and not hateful posts looking for elements that might lead to policy violations and explicitly tag them."}]
+
+    # for i in range(10):
+    #     messages.append({"role": "user", "content": instances[i]["input"]})
+    #     messages.append({"role": "assistant", "content": instances[i]["output"]})
+
+    generated_outputs = []
+
+    for i in range(0, len(instances)):
+        input_ids = tokenizer.apply_chat_template(
+            messages + [{"role": "user", "content": instances[i]["input"]}],
+            add_generation_prompt=True,
+            return_tensors="pt"
+        ).to(model.device)
+
+        terminators = [
+            tokenizer.eos_token_id,
+            tokenizer.convert_tokens_to_ids("<|eot_id|>")
+        ]
+
+        outputs = model.generate(
+            input_ids,
+            max_new_tokens=256,
+            eos_token_id=terminators,
+            do_sample=True,
+            temperature=0.6,
+            top_p=0.9,
+            num_return_sequences=3,
+        )
+
+        response = outputs[0][input_ids.shape[-1]:]
+
+        generated_outputs.append({
+            "input": instances[i]["input"],
+            # "output": instances[i]["output"],
+            "generated_output": tokenizer.decode(response, skip_special_tokens=True),
+            })
+
+    with open(output_file, "w+") as out:
+        for output in generated_outputs:
+            json.dump(output, out)
+            out.write("\n")
+    out.close()
+    print("Done!")
